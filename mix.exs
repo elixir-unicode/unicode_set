8cf43e4f014772d15136f7f4b3d3045082d6cf2b
Kip Cole <kipcole9@gmail.com>
2026-09-06 13:24:38 +1000

    Point the unicode dependency at the unicode-18 branch

--- mix.exs
+++ mix.exs
@@ -69,7 +69,7 @@ defmodule UnicodeSet.MixProject do
 
   defp deps do
     [
-      {:unicode, "~> 2.0"},
+      {:unicode, github: "elixir-unicode/unicode", branch: "unicode-18"},
       {:nimble_parsec, "~> 1.3", runtime: false},
       # {:benchee, "~> 1.0", only: :dev, optional: true},
       {:ex_doc, "~> 0.24", only: [:dev, :release], runtime: false, optional: true},
